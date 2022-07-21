545b33a3fc81b48e216126c7983a0693d1cb3548
krishnanshagarwal112 <gonkillua2680@gmail.com>
2022-07-21 10:34:26 +0530

    modules

--- 03_modules.js
+++ 03_modules.js
@@ -1,13 +1,13 @@
 // MODULES (only sharing what we want, keeping it minimize)
 // Every file in node is module
 
-
-
 const names = require("./04-names")         // importing names
 const sayHi = require("./05-utils")
 const data = require("./06-alternative_flavor")
 //console.log(names)
-console.log(data)
-sayHi("kichi")
-sayHi(names.name2)
-sayHi(names.name3)
+// console.log(data)
+// sayHi("kichi")
+// sayHi(names.name2)
+// sayHi(names.name3)
+
+require("./07-sumFunction")
