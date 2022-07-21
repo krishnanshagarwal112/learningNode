df32d5d19228b5dfb00022ec2242a54b4522b2b7
krishnanshagarwal112 <gonkillua2680@gmail.com>
2022-07-21 13:13:20 +0530

    fs async

---
+++ 10_fs_sync.js
@@ -0,0 +1,16 @@
+const {readFileSync, writeFileSync} = require("fs")
+
+const first = readFileSync('./content/first.txt','utf8')
+const second = readFileSync('./content/second.txt','utf8')
+
+writeFileSync(
+    './content/result.txt', 
+    `Chelsea is the best football club`,
+    {flag: 'w'} 
+    )
+
+const third = readFileSync('./content/result.txt','utf8')
+
+console.log(first);
+console.log(second);
+console.log(third);
