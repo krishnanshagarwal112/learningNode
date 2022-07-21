72090c1a3254819bbffbe61078c22f5ac6659d2d
krishnanshagarwal112 <gonkillua2680@gmail.com>
2022-07-21 10:24:10 +0530

    modules

--- 03_modules.js
+++ 03_modules.js
@@ -4,10 +4,10 @@
 
 
 const names = require("./04-names")         // importing names
-
 const sayHi = require("./05-utils")
+const data = require("./06-alternative_flavor")
 //console.log(names)
-
+console.log(data)
 sayHi("kichi")
 sayHi(names.name2)
 sayHi(names.name3)
