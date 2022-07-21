d03b2d089994e6e13df11f6f400d0051c06cde1d
krishnanshagarwal112 <gonkillua2680@gmail.com>
2022-07-21 10:35:26 +0530

    modules

--- 03_modules.js
+++ 03_modules.js
@@ -1,5 +1,7 @@
 // MODULES (only sharing what we want, keeping it minimize)
 // Every file in node is module
+// when we require a module, and a function is running in the module, it will be executed
+
 
 const names = require("./04-names")         // importing names
 const sayHi = require("./05-utils")
