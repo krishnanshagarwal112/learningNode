8ef58f1f0f5659365af84bcc1dfe1e8cebe3a3d9
krishnanshagarwal112 <gonkillua2680@gmail.com>
2022-07-24 15:50:24 +0530

    fix

--- 11_fs_async.js
+++ 11_fs_async.js
@@ -1,27 +1,29 @@
-const {readFile, writeFile, read} = require("fs")
+const { readFile, writeFile } = require('fs')
 
-
-readFile('./content/first.txt','utf8',(err,result)=>{
-    if(err){
-        console.log(err);
-        return 
+console.log('start')
+readFile('./content/first.txt', 'utf8', (err, result) => {
+  if (err) {
+    console.log(err)
+    return
+  }
+  const first = result
+  readFile('./content/second.txt', 'utf8', (err, result) => {
+    if (err) {
+      console.log(err)
+      return
     }
-    const first = result
-    readFile('./content/second.txt','utf8',(err,result) =>{
-        if(err){
-            console.log(err);
-            return 
+    const second = result
+    writeFile(
+      './content/result-async.txt',
+      `Here is the result : ${first}, ${second}`,
+      (err, result) => {
+        if (err) {
+          console.log(err)
+          return
         }
-        const second = result
-        writeFile('./content/result-async.txt',
-        `Here is ${first}, ${second} !`,
-        {flag: 'w'},
-        (err,result) =>{
-            if(err){
-                console.log(err);
-                return
-            }
-            console.log(result);
-        } )
-    })
+        console.log('done with this task')
+      }
+    )
+  })
 })
+console.log('starting next task')
